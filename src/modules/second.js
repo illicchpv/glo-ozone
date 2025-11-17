eb83cab68a148b3a11b95c283c013d2d3b047f78
illicchpv <illich58@mail.ru>
2025-11-17 10:33:58 +0300

    перед second -> load

--- src/modules/second.js
+++ src/modules/second.js
@@ -1,22 +1,24 @@
 import getData from './getData.js';
 import postData from './postData.js';
+import renderGoods from './renderGoods.js';
 
 const second = () => {
   const cartBtn = document.getElementById('cart');
 
   getData('second').then((data) => {
-    console.log('second getData: ', data);
+    // console.log('second getData: ', data);
+    renderGoods(data);
   });
 
-  // создание товара и получение обновленного списка
-  cartBtn.addEventListener('click', () => {
-    postData().then((data) => {
-      console.log('second postData data: ', data);
-      getData('second').then((data) => {
-        console.log('second1111 getData data: ', data);
-      });
-    });
-  });
+  // // создание товара и получение обновленного списка
+  // cartBtn.addEventListener('click', () => {
+  //   postData().then((data) => {
+  //     console.log('second postData data: ', data);
+  //     getData('second').then((data) => {
+  //       console.log('second1111 getData data: ', data);
+  //     });
+  //   });
+  // });
 };
 
 export default second;
